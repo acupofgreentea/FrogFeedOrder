7d8167ef01a4f42430f5da3686dd307f70619e92
Akın Yıldırım <akinyildirimdev@gmail.com>
2024-08-06 16:48:48 +0300

    haptic

---
+++ Assets/_Project/Scripts/Managers/HapticManager.cs
@@ -0,0 +1,32 @@
+﻿using CandyCoded.HapticFeedback;
+using UnityEngine;
+
+public static class HapticManager
+{
+    public static void LightHaptic()
+    {
+        if (DataManager.Haptic == false)
+            return;
+        
+        Debug.LogError("light haptic");
+        HapticFeedback.LightFeedback();
+    }
+    
+    public static void MediumHaptic()
+    {
+        if (DataManager.Haptic == false)
+            return;
+        
+        Debug.LogError("medium haptic");
+        HapticFeedback.MediumFeedback();
+    }
+    
+    public static void HeavyHaptic()
+    {
+        if (DataManager.Haptic == false)
+            return;
+        
+        Debug.LogError("heavy haptic");
+        HapticFeedback.HeavyFeedback();
+    }
+}
